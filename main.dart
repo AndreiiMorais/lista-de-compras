e212bbf1138eb854256efe4ecba1ab59ee35f443
Andrei Silveira Morais <andrei.morais@outlook.com>
2021-12-12 23:57:20 -0300

    Update main.dart
    corrigido o problema de adicionar em branco.

--- main.dart
+++ main.dart
@@ -3,24 +3,23 @@ import 'dart:io';
 main() {
   List<String> produtos = [];
   bool condicao = true;
-  String texto = "sair";
+  String texto;
   while (condicao) {
     print("Adicione um produto: ");
     String? text = stdin.readLineSync();
-    if (text == null) {
-      print("não é possivel deixar em branco, tente novamente:");
-      String? text = stdin.readLineSync();
+    if (text == null || text.isNotEmpty) {
+      if (text == "sair") {
+        print("Programa encerrado: ");
+        condicao = false;
+      } else if (text == "imprimir") {
+        print(produtos);
+        print("\n");
+      } else {
+        texto = text.toString();
+        produtos.add(texto);
+      }
     } else {
-      texto = text;
-    }
-    if (texto == "sair") {
-      print("Programa encerrado: ");
-      condicao = false;
-    } else if (texto == "imprimir") {
-      print(produtos);
-      print("\n");
-    } else {
-      produtos.add(texto);
+      print("Nao é possivel deixar em branco, ou caractere desconhecido");
     }
   }
 }
